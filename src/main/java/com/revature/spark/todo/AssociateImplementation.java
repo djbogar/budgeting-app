4e498f572eb28b10ff77b7380dc495d730f55183
djbogar <46689626+djbogar@users.noreply.github.com>
2019-02-14 12:58:47 -0500

    Project -1 Final

--- src/main/java/com/revature/spark/todo/AssociateImplementation.java
+++ src/main/java/com/revature/spark/todo/AssociateImplementation.java
@@ -22,7 +22,11 @@ public class AssociateImplementation {
 	 * @return
 	 */
 	public Double sum(List<Expense> expenses) {
-		return null;
+		double sum = 0;
+		for(Expense i : expenses) {
+			sum+= i.getCost();
+		}
+		return sum;
 	}
 
 	/**
@@ -32,7 +36,13 @@ public class AssociateImplementation {
 	 * @return
 	 */
 	public Double min(List<Expense> expenses) {
-		return null;
+		double min = Double.MAX_VALUE;
+		for(Expense i : expenses) {
+			if(min > i.getCost()) {
+				min = i.getCost();
+			}
+		}
+		return min;
 	}
 
 	/**
@@ -41,8 +51,14 @@ public class AssociateImplementation {
 	 * @param calls
 	 * @return
 	 */
-	public Double max(List<Expense> expenses) {
-		return null;
+	public Double max(List<Expense> expenses) {	
+		double max = 0;
+		for(Expense i : expenses) {
+			if(max < i.getCost()) {
+				max = i.getCost();
+			}
+		}
+		return max;
 	}
 
 	/**
@@ -52,7 +68,11 @@ public class AssociateImplementation {
 	 * @return
 	 */
 	public Double avg(List<Expense> expenses) {
-		return null;
+		double sum = 0;
+		for(Expense i : expenses) {
+			sum+= i.getCost();
+		}
+		return sum/expenses.size();
 	}
 
 	/**
@@ -62,7 +82,37 @@ public class AssociateImplementation {
 	 * @return
 	 */
 	public Double median(List<Expense> expenses) {
-		return null;
+		double[] medianArray = new double[expenses.size()];
+		int count = 0;
+		for(Expense i : expenses) {
+			medianArray[count] = i.getCost();
+			count++;
+		}
+		int size = expenses.size();
+		for(int i = 0; i < size - 1; i++) {
+			for(int j = 0; j < size - i - 1; j++) {
+				if(medianArray[j] > medianArray[j+1]) {
+					double temp = medianArray[j];
+					medianArray[j] = medianArray[j+1];
+					medianArray[j+1] = temp;
+				}
+			}
+		}
+//		for(int i= 0; i < medianArray.length; i++) {
+//			System.out.print(medianArray[i] + ", ");
+//		}
+
+		if(medianArray.length%2 == 0) {
+			double medianValue = 0;
+			medianValue = (medianArray[medianArray.length/2] + medianArray[(medianArray.length/2)-1])/2;
+			return medianValue;
+			
+		}
+		else {
+			double medianValue = 0;
+			medianValue = medianArray[medianArray.length/2];
+			return medianValue;
+		}
 	}
 
 	/**
